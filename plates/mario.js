70cd18262996c44eda232c46d6d0abf35ef3c705
Aituglo <contact@aituglo.com>
2020-07-19 15:38:12 +0200

    Add mario

---
+++ plates/mario.js
@@ -0,0 +1,608 @@
+module.exports = {
+    board: [
+        {
+            //GO
+            name: "GO",
+            position: 0,
+            type: "go",
+            url: "https://i.imgur.com/zlsuZeT.jpg"
+        },
+        {
+            //Mediterranean Avenue
+            name: "Rambouille",
+            cost: 60,
+            upgrade: 50,
+            position: 1,
+            group: "purple",
+            rent: [
+                2, 10, 30, 90, 160, 250,
+            ],
+            type: "property",
+            url: "images/mario/rambouille.jpg",
+            color: "#5E3C9F",
+        },
+        {
+            //Community Chest
+            name: "Caisse de Communauté",
+            position: 2,
+            type: "chance",
+            url: "images/mario/commu.png"
+        },
+        {
+            //Baltic Ave
+            name: "Bryan",
+            cost: 60,
+            upgrade: 50,
+            position: 3,
+            group: "purple",
+            rent: [
+                4, 20, 60, 180, 320, 450,
+            ],
+            type: "property",
+            url: "images/mario/bryan.jpg",
+            color: "#5E3C9F",
+        },
+        {
+            //Income Tax
+            name: "Taxe",
+            position: 4,
+            type: "income-tax",
+            url: "https://i.imgur.com/1E3cLRO.png"
+        },
+        {
+            //Reading Railroad
+            name: "Marion",
+            cost: 200,
+            base_rent: 25,
+            position: 5,
+            type: "rr",
+            url: "images/mario/marion.jpg",
+            color: "#8B4513",
+        },
+        {
+            //Connecticut Avenue
+            name: "Baptiste",
+            cost: 120,
+            upgrade: 50,
+            position: 9,
+            group: "light-blue",
+            rent: [
+                6, 30, 90, 270, 400, 550,
+            ],
+            type: "property",
+            url: "images/mario/baptiste.jpg",
+            color: "#9eeafb"
+        },
+        {
+            //Chance
+            name: "Chance",
+            position: 7,
+            type: "chance",
+            url: "images/mario/chance.png"
+        },
+        {
+            //Vermont Avenue
+            name: "Victor",
+            cost: 100,
+            upgrade: 50,
+            position: 8,
+            group: "light-blue",
+            rent: [
+                6, 30, 90, 270, 400, 550,
+            ],
+            type: "property",
+            url: "images/mario/victor.jpg",
+            color: "#9eeafb"
+        },
+        {
+            //Oriental Ave
+            name: "Gauthier",
+            cost: 100,
+            upgrade: 50,
+            position: 6,
+            group: "light-blue",
+            rent: [
+                8, 40, 100, 300, 450, 600,
+            ],
+            type: "property",
+            url: "images/mario/gauthier.jpg",
+            color: "#9eeafb"
+        },
+        {
+            name: "Prison",
+            position: 10,
+            type: "jail",
+            url: "https://i.imgur.com/UjkDuYN.jpg"
+        },
+        {
+            //St Charles Place
+            name: "Pierre",
+            cost: 140,
+            upgrade: 100,
+            position: 11,
+            group: "pink",
+            rent: [
+                10, 50, 150, 450, 625, 750,
+            ],
+            type: "property",
+            url: "images/mario/pierre.jpg",
+            color: "#D13F9B",
+        },
+        {
+            //Electric Company
+            name: "Romain",
+            cost: 150,
+            position: 12,
+            group: "utility",
+            type: "utility",
+            url: "images/mario/romain.jpg",
+            color: "#FBB677"
+        },
+        {
+            //States Avenue
+            name: "Nasrédine",
+            cost: 140,
+            upgrade: 100,
+            position: 13,
+            group: "pink",
+            rent: [
+                10, 50, 150, 450, 625, 750,
+            ],
+            type: "property",
+            url: "images/mario/nas.jpg",
+            color: "#D13F9B",
+        },
+        {
+            //Virginia Avenue
+            name: "Louis",
+            cost: 160,
+            upgrade: 100,
+            position: 14,
+            group: "pink",
+            rent: [
+                12, 60, 180, 500, 700, 900,
+            ],
+            type: "property",
+            url: "images/mario/louis.jpg",
+            color: "#D13F9B",
+        },
+        {
+            //Pennsylvania Railroad
+            name: "Kelly",
+            cost: 200,
+            base_rent: 25,
+            position: 15,
+            type: "rr",
+            url: "images/mario/kelly.jpg",
+            color: "#8B4513",
+        },
+        {
+            //Tennessee Avenue
+            name: "Lou-anne",
+            cost: 180,
+            upgrade: 100,
+            position: 18,
+            group: "orange",
+            rent: [
+                14, 70, 200, 550, 750, 950,
+            ],
+            type: "property",
+            url: "images/mario/loulou.jpg",
+            color: "#FFA500",
+        },
+        {
+            //Community Chest
+            name: "Caisse de Communauté",
+            position: 17,
+            type: "chest",
+            url: "images/mario/commu.png"
+        },
+        {
+            //St James Place
+            name: "Alexis",
+            cost: 180,
+            upgrade: 100,
+            position: 16,
+            group: "orange",
+            rent: [
+                14, 70, 200, 550, 750, 950,
+            ],
+            type: "property",
+            url: "images/mario/alexis.jpg",
+            color: "#FFA500",
+        },
+        {
+            //New York Avenue
+            name: "Camélia",
+            cost: 200,
+            upgrade: 100,
+            position: 19,
+            group: "orange",
+            rent: [
+                16, 80, 220, 600, 800, 1000,
+            ],
+            type: "property",
+            url: "images/mario/camelia.jpg",
+            color: "#FFA500",
+        },
+        {
+            //Free Parking
+            name: "Parking",
+            position: 20,
+            type: "parking",
+            url: "images/mario/parking.png"
+        },
+        {
+            //Kentucky Avenue
+            name: "Margaux",
+            cost: 220,
+            upgrade: 150,
+            position: 21,
+            group: "red",
+            rent: [
+                18, 90, 250, 700, 875, 1050,
+            ],
+            type: "property",
+            url: "images/mario/margaux.jpg",
+            color: "#ff3f3f"
+        },
+        {
+            //Chance
+            name: "Chance",
+            position: 22,
+            type: "chance",
+            url: "images/mario/chance.png"
+        },
+        {
+            //Indiana Avenue
+            name: "Céline",
+            cost: 220,
+            upgrade: 150,
+            position: 23,
+            group: "red",
+            rent: [
+                18, 90, 250, 700, 875, 1050,
+            ],
+            type: "property",
+            url: "images/mario/celine.jpg",
+            color: "#ff3f3f"
+        },
+        {
+            //Illinois Avenue
+            name: "Manon M",
+            cost: 240,
+            upgrade: 150,
+            position: 24,
+            group: "red",
+            rent: [
+                20, 100, 300, 750, 925, 1100,
+            ],
+            type: "property",
+            url: "images/mario/manonm.jpg",
+            color: "#ff3f3f"
+        },
+        {
+            //B&0 Railroad
+            name: "Léa",
+            cost: 200,
+            base_rent: 25,
+            position: 25,
+            type: "rr",
+            url: "images/mario/lea.jpg",
+            color: "#8B4513",
+        },
+        {
+            //Ventnor Avenue
+            name: "Gillou",
+            cost: 260,
+            upgrade: 150,
+            position: 27,
+            group: "yellow",
+            rent: [
+                22, 110, 330, 800, 975, 1150,
+            ],
+            type: "property",
+            url: "images/mario/gillou.jpg",
+            color: "#fff852"
+        },
+        {
+            //Marvin Gardens
+            name: "Poupe",
+            cost: 280,
+            upgrade: 150,
+            position: 29,
+            group: "yellow",
+            rent: [
+                22, 110, 330, 800, 975, 1150,
+            ],
+            type: "property",
+            url: "images/mario/poupe.jpg",
+            color: "#fff852"
+        },
+        {
+            //Water Works
+            name: "Lucas Milas",
+            cost: 150,
+            position: 28,
+            group: "utility",
+            type: "utility",
+            url: "images/mario/milas.jpg",
+            color: "#FBB677"
+        },
+        {
+            //Atlantic Avenue
+            name: "Melissa",
+            cost: 260,
+            upgrade: 150,
+            position: 26,
+            group: "yellow",
+            rent: [
+                24, 120, 360, 850, 1025, 1200,
+            ],
+            type: "property",
+            url: "http://melissa.inglart.free.fr/CV/photoID.jpg",
+            color: "#fff852"
+        },
+        {
+            name: "Tu vas en prison",
+            position: 30,
+            type: "go-to-jail",
+            url: "https://i.imgur.com/B4pTwgp.jpg"
+        },
+        {
+            //Pacific Avenue
+            name: "Lucas Puig",
+            cost: 300,
+            upgrade: 200,
+            position: 31,
+            group: "green",
+            rent: [
+                26, 130, 390, 900, 1100, 1275,
+            ],
+            type: "property",
+            url: "images/mario/lucas.jpg",
+            color: "#008000",
+        },
+        {
+            //North Carolina Avenue
+            name: "Hugo",
+            cost: 300,
+            upgrade: 200,
+            position: 32,
+            group: "green",
+            rent: [
+                26, 130, 390, 900, 1100, 1275,
+            ],
+            type: "property",
+            url: "images/mario/hugo.jpg",
+            color: "#008000",
+        },
+        {
+            //Community Chest
+            name: "Caisse de Communauté",
+            position: 33,
+            type: "chest",
+            url: "images/mario/commu.png"
+        },
+        {
+            //Pennsylvania Avenue
+            name: "Cassim",
+            cost: 320,
+            upgrade: 200,
+            position: 34,
+            group: "green",
+            rent: [
+                28, 150, 450, 1000, 1200, 1400,
+            ],
+            type: "property",
+            url: "images/mario/cassim.jpg",
+            color: "#008000",
+        },
+        {
+            //Short Line
+            name: "Manon L",
+            cost: 200,
+            base_rent: 25,
+            position: 35,
+            type: "rr",
+            url: "images/mario/manon.jpg",
+            color: "#8B4513",
+        },
+        {
+            //Chance
+            name: "Chance",
+            position: 36,
+            type: "chance",
+            url: "images/mario/chance.png"
+        },
+        {
+            //Park Place
+            name: "Béatrice",
+            cost: 350,
+            upgrade: 200,
+            position: 37,
+            group: "dark-blue",
+            rent: [
+                35, 175, 500, 1100, 1300, 1500,
+            ],
+            type: "property",
+            url: "images/mario/bea.jpg",
+            color: "#2148ff",
+        },
+        {
+            //Luxury Tax
+            name: "Evasion de Taxe",
+            position: 38,
+            type: "lux-tax",
+            url: "https://i.imgur.com/332sOlL.jpg"
+        },
+        {
+            //Boardwalk
+            name: "Rémi",
+            cost: 400,
+            upgrade: 200,
+            position: 39,
+            group: "dark-blue",
+            rent: [
+                50, 200, 600, 1400, 1700, 2000,
+            ],
+            type: "property",
+            url: "images/mario/remi.jpg",
+            color: "#2148ff",
+        },
+    ],
+    chance: [
+        {
+            name: "Va à la case départ (Gagne $200).",
+            type: "simple_move",
+            position: 0
+        },
+        {
+            name: "Va voir Camélia.",
+            type: "simple_move",
+            position: 24
+        },
+        {
+            name: "Va voir Pierre, il fait un malaise..",
+            type: "simple_move",
+            position: 11,
+            //if you pass Go
+        },
+        {
+            name: "Rejoint Romain",
+            type: "nearest_utility",
+            extra: "no_go",
+            //either tesla or spacex
+        },
+        {
+            name: "Avancez à la prochaine bgette, si elle n'est pas prise, vous pouvez l'acheter !",
+            type: "nearest_rr",
+        },
+        {
+            name: "Ouverture de la Fata ! + $50.",
+            type: "simple_bank_pay",
+            amount: 50,
+        },
+        {
+            name: "Reculez de 3 cases.",
+            type: "move_amount",
+            amount: -3,
+
+        },
+        {
+            name: "Aller en prison",
+            type: "go_to_jail"
+        },
+        {
+            name: "Payez 15$ pour éviter une colle avec Soudan",
+            type: "simple_bank_pay",
+            amount: -15,
+        },
+        {
+            name: "Allez voir cette bgette de Marion",
+            type: "simple_move",
+            position: 5
+        },
+        {
+            name: "Souveton à quelque chose à vous dire",
+            type: "simple_move",
+            position: 39
+        },
+        {
+            name: "Vous avez tricher aux formules trigos. Payez à tous $50.",
+            type: "pay_all_players",
+            amount: 50,
+        },
+        {
+            name: "Vous avez colle avec Brabant, + $150.",
+            type: "simple_bank_pay",
+            amount: 150,
+        },
+        {
+            name: "CHICKEN BURGER ! +$100",
+            type: "simple_bank_pay",
+            amount: 100,
+        },
+    ],
+    chest: [
+        {
+            name: "Reviens au départ et gagne $200",
+            type: "simple_move",
+            position: 0,
+        },
+        {
+            name: "Tu as réussi un exo de souveton, + $200",
+            type: "simple_bank_pay",
+            amount: 200,
+        },
+        {
+            name: "Tu dois payer la cantine... - $50",
+            type: "simple_bank_pay",
+            amount: -50,
+        },
+        {
+            name: "Tu as survécu une heure avec OCF, +$50",
+            type: "simple_bank_pay",
+            amount: 50,
+        },
+        // {
+        //     name: "Get out of Jail Free",
+        //     type: "chest"
+        // },
+        {
+            name: "Tu vas en prison ...",
+            type: "go_to_jail"
+        },
+        {
+            name: "Tu t'es endormi en Français... -$50.",
+            type: "pay_all_players",
+            amount: -50,
+        },
+        {
+            name: "Tu as réussi à tenir 2 h sans te faire prendre par Chemimi, +$100",
+            type: "simple_bank_pay",
+            amount: 100,
+        },
+        {
+            name: "Tu as survécu une heure avec Delcroix $20",
+            type: "simple_bank_pay",
+            amount: 20,
+        },
+        {
+            name: "C'est le Zinzin ! Voici $10",
+            type: "simple_bank_pay",
+            amount: 10,
+        },
+        {
+            name: "Tu as gérer avec Souveton, tu gagnes $100",
+            type: "simple_bank_pay",
+            amount: 100,
+        },
+        {
+            name: "Tu viens de rater ta colle ... -$100",
+            type: "simple_bank_pay",
+            amount: -100,
+        },
+        {
+            name: "Tu dois payer l'internat $150",
+            type: "simple_bank_pay",
+            amount: -150,
+        },
+        {
+            name: "Tu reçois $25 pour avoir supporter Delcroix",
+            type: "simple_bank_pay",
+            amount: 25,
+        },
+        {
+            name: "T'as de plus belles fesses que Melissa, tu gagnes $10",
+            type: "simple_bank_pay",
+            amount: 10,
+        },
+        {
+            name: "Vous gagnez $100",
+            type: "simple_bank_pay",
+            amount: 100,
+        },
+    ]
+
+};
